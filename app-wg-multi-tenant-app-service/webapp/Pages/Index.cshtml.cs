b30cb73f119d4f4484e4510b3cbf8557b14ab70c
peterlil <29121387+peterlil@users.noreply.github.com>
2022-02-15 15:19:40 +0100

    app-gw-multi-tenant-app-service

---
+++ app-wg-multi-tenant-app-service/webapp/Pages/Index.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+
+namespace webapp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration _configuration;
+
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+            ApiUri = String.Empty;
+            WeatherSummary = String.Empty;
+        }
+
+        public void OnGet()
+        {
+            ApiUri = _configuration["BackendAPI"];
+
+            HttpClient httpClient = new HttpClient { BaseAddress = new Uri(ApiUri) };
+            Task<string> taskReturn =  httpClient.GetStringAsync("/WeatherForecast");
+            taskReturn.Wait();
+            WeatherSummary = taskReturn.Result;
+        }
+
+        public string ApiUri{ get; set; }
+
+        public string WeatherSummary { get; set; }
+    }
+}
