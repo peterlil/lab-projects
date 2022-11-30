7868542d3165c37e741f39d07c799fcbc18544fb
peterlil <29121387+peterlil@users.noreply.github.com>
2022-11-30 09:42:50 +0100

    api management shgw container mod

--- funcapi-python-flask/cd.md
+++ funcapi-python-flask/cd.md
@@ -3,7 +3,7 @@
 Create the service principal for the workflow.
 
 ```PowerShell
-$appregName = "funcapi-python-flask-cd"
+$appregName = "ext-funcapi-python-flask-cd"
 $subscriptionId = (az account show --query "id" -o tsv)
 
 az ad sp create-for-rbac --name $appregName --role contributor `
--- msal-consolapp-launch-excel/Constants.cs
+++ msal-consolapp-launch-excel/Constants.cs
@@ -2,13 +2,21 @@ namespace MsalConsole
 {
     internal class Constants 
     {
-        public const string Tenant = "16b3c013-d300-468d-ac64-7eda0820b6d3"; // Microsoft non-prod tenant
+        //public const string Tenant = "16b3c013-d300-468d-ac64-7eda0820b6d3"; // Microsoft non-prod tenant
         //public const string ClientId = "bf14828b-a3c0-4bda-bb35-42bc07eed4bf"; // peterlil-wpf-app
         public static string Instance = "https://login.microsoftonline.com/";
-        //public static string[] scopes = new string[] { "user.read" }; // The scope for API call to user.read
-        public const string ClientId = "df73c213-eb7a-466b-ae63-4d216c803eed"; // funcapi-python-flask-no-secret
-        //public static string[] scopes = new string[] { "user_impersonation" }; // The scope for API call to user_impersonation
-        public static string[] scopes = new string[] { "https://funcapi_python_flask.fdpo.onmicrosoft.com/user_impersonation" }; // The scope for API call to user_impersonation
         
+        //public static string[] scopes = new string[] { "user.read" }; // The scope for API call to user.read
+        
+        //public const string ClientId = "df73c213-eb7a-466b-ae63-4d216c803eed"; // funcapi-python-flask-no-secret
+        //public static string[] scopes = new string[] { "user_impersonation" }; // The scope for API call to user_impersonation
+        //public static string[] scopes = new string[] { "https://funcapi_python_flask.fdpo.onmicrosoft.com/user_impersonation" }; // The scope for API call to user_impersonation
+        
+
+        public const string Tenant = "7bcae91a-4325-4821-84b1-aac9d5caaa22"; // Microsoft non-prod tenant
+        public const string ClientId = "ac13dfc5-345b-49b8-bfd0-76e549aab3fb"; // funcapi-python-flask-no-secret
+        public static string[] scopes = new string[] { "https://funcapi-python-flask-api.MngEnv319828.onmicrosoft.com/user_impersonation" }; // The scope for API call to user_impersonation
+        
+
     }
 }
