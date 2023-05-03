66e6dacc2354d3cc463699e346462db1178b0ff0
peterlil <29121387+peterlil@users.noreply.github.com>
2023-05-03 16:39:15 +0200

    simple-test

---
+++ simple-tests/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+
+StringBuilderTests.Test1();
+
+public class StringBuilderTests
+{
+    public static void Test1()
+    {
+        var sb = new StringBuilder();
+        sb.Append("Hello").Append("World").Append("!");
+        Console.WriteLine(sb.ToString());
+ 
+    }
+}
