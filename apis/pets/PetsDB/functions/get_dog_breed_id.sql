f01353bd9a016ca9c917b581d1f8c04e756114f5
Peter Liljenroth <sail@live.se>
2024-10-08 08:32:07 +0200

    First version of db. empty api project

---
+++ apis/pets/PetsDB/functions/get_dog_breed_id.sql
@@ -0,0 +1,10 @@
+﻿CREATE FUNCTION [dbo].[get_dog_breed_id]
+(
+	@breed_name nvarchar(100)
+)
+RETURNS INT
+BEGIN
+	DECLARE @dog_breed_id INT;
+	SELECT @dog_breed_id = [id] FROM dbo.dog_breeds WHERE [name] = @breed_name
+	RETURN @dog_breed_id
+END;
---
+++ apis/pets/PetsDB/functions/get_dog_group_id.sql
@@ -0,0 +1,10 @@
+﻿CREATE FUNCTION [dbo].[get_dog_group_id]
+(
+	@group_name nvarchar(100)
+)
+RETURNS INT
+BEGIN
+	DECLARE @dog_group_id INT;
+	SELECT @dog_group_id = [id] FROM dbo.dog_groups WHERE [name] = @group_name
+	RETURN @dog_group_id
+END;
